3813917f23fed19d925fd2a122a8e7be4447f8a4
Alexander Hoinville <ahoin001@gmail.com>
2020-04-22 23:47:56 -0400

    Set up routes and model and post controller

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,21 @@
-const express = require('express')
+// ? TS import syntax so TS has better support and auto complete
+import express, { Request, Response, NextFunction } from 'express'
+import todoRoutes from './routes/todo-routes'
+import { json } from 'body-parser'
 
 const app = express();
+
+// parses body of all incoming requests and extracts json data to place in req.body
+app.use(json())
+
+// ? All Requests starting with todos will use imported routes
+app.use('/todos', todoRoutes)
+
+// ? handles error if any other middleware has error
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+
+    res.status(500).json({ message: err.message })
+
+})
 
 app.listen(3000)
